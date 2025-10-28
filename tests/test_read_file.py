d16d2f841df34100d6ccec382383850b10cd788a
Valor <ibbyissa001@gmail.com>
2025-10-28 07:28:17 +0100

    Formatted code to PEP8 standards

--- tests/test_read_file.py
+++ tests/test_read_file.py
@@ -2,13 +2,17 @@ import unittest
 import tempfile
 from utils.reader import read_text_from_file
 
+
 class TestFileReaderFunction(unittest.TestCase):
     def test_reader(self):
-        with tempfile.NamedTemporaryFile(mode="w+", suffix=".txt", delete=False) as temp:
+        with tempfile.NamedTemporaryFile(
+            mode="w+", suffix=".txt", delete=False
+        ) as temp:
             temp.write("Test Case.")
             temp.flush()
             result = read_text_from_file(temp.name)
             self.assertEqual(result.strip(), "Test Case.")
+
 
 if __name__ == "__main__":
     unittest.main()
